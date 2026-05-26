86ec31ed9716510377b347b7298ac966e0f21d6e
Dylan Tay <251165L@mymail.nyp.edu.sg>
2026-05-26 19:23:35 +0800

    added keep-alive

--- role_sync_bot.py
+++ role_sync_bot.py
@@ -2,7 +2,19 @@ import discord
 from discord.ext import commands, tasks
 import aiohttp
 import asyncio
+from flask import Flask
+from threading import Thread
 
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive!"
+
+def run_http():
+    app.run(host='0.0.0.0', port=8080)
+
+Thread(target=run_http).start()
 # ===== CONFIGURATION =====
 import os
 
